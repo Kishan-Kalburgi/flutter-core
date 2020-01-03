0c33eb5fc63036d98a26f82a11fc46e47a2bb9a4
kishan <kishan.ks@hotmail.com>
2020-01-03 00:00:41 -0500

    final

---
+++ lib/text_control.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import './text_output.dart';
+
+class TextControl extends StatefulWidget {
+  @override
+  _TextControlState createState() => _TextControlState();
+}
+
+class _TextControlState extends State<TextControl> {
+  String _mainText = 'This is my first Assignment';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        RaisedButton(
+          onPressed: () {
+            setState(() {
+              _mainText = 'This changed!';
+            });
+          },
+          child: Text('Change Text'),
+        ),
+        TextOutput(_mainText)
+      ],
+    );
+  }
+}
